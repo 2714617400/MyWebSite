853b3ee606ed9e6559bd804dec0d7b4c13739927
hejiaqun <17774657825@163.com>
2020-02-16 15:28:59 +0800

    测试七牛云资源

--- src/App.vue
+++ src/App.vue
@@ -50,11 +50,11 @@ export default {
           theme: 'pic'
         },
         {
-          title: 'secret base~君がくれたもの~',
-          artist: 'Silent Siren',
-          src: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/hikarunara.mp3',
-          pic: 'https://cn-south-17-aplayer-46154810.oss.dogecdn.com/hikarunara.jpg',
-          theme: 'pic'
+          title: '四季之歌',
+          artist: 'nj',
+          src: 'http://q5ry2obn4.bkt.clouddn.com/sjzg.mp3',
+          pic: 'none',
+          theme: 'none'
         },
       ]
     }
